8df04d4f875804d1acd4bfbe60f4ee105e001ed7
slowbs <poison_word@hotmail.com>
2019-03-18 16:09:12 +0700

    edit label work
    profile edit label work by add class label-class

--- common/themes/adminlte/layouts/header.php
+++ common/themes/adminlte/layouts/header.php
@@ -263,7 +263,7 @@ use yii\helpers\Html;
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                <a href="profile" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
                                 <?= Html::a(
